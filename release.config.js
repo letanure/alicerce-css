e8f64e5fea6dd29c3fe01bbf123094f353a9f538
Luiz tanure <letanure@gmail.com>
2019-05-03 17:03:03 +0200

    chore: add circleci config

--- release.config.js
+++ release.config.js
@@ -5,6 +5,6 @@ module.exports ={
     '@semantic-release/github',
     '@semantic-release/changelog',
     '@semantic-release/npm',
-    ['@semantic-release/git', {message: `chore(release): \${nextRelease.version}`}],
+    ['@semantic-release/git', {message: `chore(release): \${nextRelease.version} [skip ci]`}],
   ],
 }
